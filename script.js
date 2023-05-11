bd8883e7cadc56da1da81763af5dc8939033de2b
chamalruwanjith <83910595+chamalruwanjith@users.noreply.github.com>
2023-05-12 01:51:04 +0530

    Add files via upload
    init

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 var runStart = 0;
 function keyCheck(event) {
     //Enter Key
-    if (event.which == 11) {
+    if (event.which == 13) {
         if (runWorkerId == 0) {
             createBlockId = setInterval(createBlock, 100);
             moveBlockId = setInterval(moveBlocks, 100);
             runWorkerId = setInterval(run, 100);
-            runStart - 1;
+            runStart = 1;
             runSound.play();
             backgroundWorkerId = setInterval(moveBackground, 100);
             ScoreWorkerId = setInterval(updateScore, 100);
         }
-
-
-
     }
 
     //Space Key
@@ -27,10 +24,16 @@ function keyCheck(event) {
               jumpSound.play();
             }
         }
-
-
-
     }
+}
+function startGame() {
+    document.getElementById("startPage").style.display = "none";
+    document.getElementById("background").style.display = "block";
+    document.getElementById("score").innerHTML = "0";
+    document.getElementById("gameOver").style.visibility = "hidden";
+
+    // Add event listener to document for key check
+    document.addEventListener("keyup", keyCheck);
 }
 
 //Create Block
@@ -38,14 +41,15 @@ var blockMarginLeft = 1000;
 var createBlockId = 0;
 var blockId = 1;
 function createBlock(){
-    var Block = document.createElement("div");
-    Block.className = "block";
+    var block = document.createElement("div");
+    block.className = "block";
 
-    blockId.Id = "block"+blockId;
+    block.id = "block"+blockId;
     blockId++;
 
     var gap = Math.random()*(1000-400)+400;
     blockMarginLeft = blockMarginLeft+gap;
+
     block.style.marginLeft = blockMarginLeft+"px";
 
     document.getElementById("background").appendChild(block);
@@ -55,35 +59,38 @@ function createBlock(){
 //Move Blocks
 var moveBlockId = 0;
 function moveBlocks(){
-    for(var i = 1; i<= blockId; i++){
-        var currentBlock = document.getElementById("block"+i);
-        var currentMarginLeft = currentBlock.style.marginLeft;
-        var newMarginLeft = parseInt(currentMarginLeft)-20;
-        currentBlock.style.marginLeft = newMarginLeft+"px";
+    for(var i = 1; i<=blockId;i++){
+       var currentBlock = document.getElementById("block"+i);
+       var currentMargineLeft = currentBlock.style.marginLeft;
+       var newMargineLeft = parseInt(currentMargineLeft)-20;
+       currentBlock.style.marginLeft = newMargineLeft +"px";
 
-        if(newMarginLeft<=135){
-            if(newMarginLeft>-35){
-                if(knightMarginTop<580){
-                    if(knightMarginTop>520){
-                        clearInterval(runWorkerId);
-                        runSound.pause();
-                        clearInterval(jumpWorkerId);
-                        jumpWorkerId = -1;
-                        clearInterval(backgroundWorkerId);
-                        clearInterval(ScoreWorkerId);
-                        clearInterval(createBlockId);
-                        clearInterval(moveBlockId);
+       if(newMargineLeft<=131){
+          if(newMargineLeft>=31){
+              if(knightMarginTop<=370){
+                 if(kinghtMarginTop>310){
+                     clearInterval(runWorkerId);
+                     runSound.pause();
+                     clearInterval(jumpWorkerId);
+                     jumpWorkerId = 1;
+                     clearInterval(backgroundWorkerId);
+                     clearInterval(scoreWorkerId);
+                     clearInterval(createBlockId);
+                     clearInterval(moveBlockId);
 
-                        deadWorkerId = setInterval(dead, 100);
-                        deadSound.play();
-                    }
-                }
-            }
-        }
+
+                    deadWorkerId = setInterval(dead, 100);
+                    deadSound.play();
+
+                   }
+               }
+           }
+       }
+
     }
 }
 
-var runSound = new Audio("jump.mp3");
+var runSound = new Audio("run.mp3");
 runSound.loop = true;
 //Run Function
 var knight = document.getElementById("knight");
@@ -91,39 +98,37 @@ var runImageNumber = 1;
 var runWorkerId = 0;
 function run() {
     runImageNumber++;
-    if (runImageNumber == 11) {
+    if (runImageNumber == 9) {
         runImageNumber = 1;
     }
     knight.src = "image/Run ("+runImageNumber+").png";
 }
 
-var jumpSound = new Audio("jump.mp3");
+var jumpSound = new Audio("jump.mp3")
 //Jump Function
 var jumpImageNumber = 1;
 var jumpWorkerId = 0;
-var knightMarginTop = 580;
-function jump() {
+var knightMarginTop = 500;
+function jump(){
     jumpImageNumber++;
     if(jumpImageNumber<=7){
-    knightMarginTop = knightMarginTop-30;
-    knight.style.marginTop = knightMarginTop+"px";
+        knightMarginTop = knightMarginTop - 30;
+        knight.style.marginTop = knightMarginTop+"px";
     }
-
     if(jumpImageNumber>=8){
-    knightMarginTop = knightMarginTop+30;
-    knight.style.marginTop = knightMarginTop+"PX";
+        knightMarginTop = knightMarginTop + 30;
+        knight.style.marginTop = knightMarginTop+"px";
     }
+    if(jumpImageNumber==13){
+        jumpImageNumber=1;
 
-    if(jumpImageNumber==11){
-    jumpImageNumber = 1;
+        clearInterval(jumpWorkerId);
+        jumpWorkerId = 0;
 
-    clearInterval(jumpWorkerId);
-    jumpWorkerId = 0;
-
-    runWorkerId = setInterval(run, 100);
-    runSound.play();
-  }
-  knight.src = "image/Jump ("+jumpImageNumber+").png";
+        runWorkerId = setInterval(run, 100);
+        runSound.play();
+    }
+    knight.src = "Jump ("+jumpImageNumber+").png";
 }
 
 //Move Background
@@ -131,18 +136,18 @@ var background = document.getElementById("background");
 var backgroundX = 0;
 var backgroundWorkerId = 0;
 function moveBackground(){
-    backgroundX = backgroundX = 20;
-    backgroundWorkerId.style.backgroundPositionX = backgroundX+"px";
+    backgroundX = backgroundX-20;
+    background.style.backgroundPositionX = backgroundX+"px";
 }
 
 //Update Score
 var score = document.getElementById("score");
 var newScore = 0;
-var ScoreWorkerId = 0;
+var backgroundWorkerId = 0;
 function updateScore(){
     newScore++;
     score.innerHTML = newScore;
-}
+}  
 
 var deadSound = new Audio("dead.mp3");
 //Dead Function
@@ -153,7 +158,7 @@ function dead(){
     if(deadImageNumber == 11){
         deadImageNumber = 0;
 
-        knight.style.marginTop = "580px";
+        knight.style.marginTop = "600px";
         document.getElementById("gameOver").style.visibility = "visible";
         document.getElementById("endScore").innerHTML = newScore;
     }
